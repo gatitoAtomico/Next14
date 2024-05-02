2e518c0d22c7630ebe59d4395b9813c2f249d8c3
unknown <sup.georgios@gmail.com>
2024-05-02 18:54:05 +0300

    generate static and dynamic metadata

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,8 +1,18 @@
-export default function ProductDetails({
-  params,
-}: {
-  params: { productId: string };
-}) {
+import { Metadata } from "next";
+
+type Props = {
+  params: {
+    productId: String;
+  };
+};
+
+//generateMetadata can also be an sync function
+export const generateMetadata = ({ params }: Props): Metadata => {
+  return {
+    title: `Product ${params.productId}`,
+  };
+};
+export default function ProductDetails({ params }: Props) {
   const { productId } = params;
 
   return <h1>Details about product {productId}</h1>;
