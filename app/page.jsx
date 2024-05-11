2329f9aa4ac50d058fc1a4e6ad00a30efe18d9c8
unknown <sup.georgios@gmail.com>
2024-05-11 19:17:21 +0300

    minor changes

--- app/page.jsx
+++ app/page.jsx
@@ -49,6 +49,27 @@ export default function Page() {
       <Row>
         <Link href={"/products"}>Products1</Link>
       </Row>
+      <Row>{jokeMessage}</Row>
+      <Row>
+        <Link href={"/blog"}>Blog1</Link>
+      </Row>
+      <Row>
+        <Link href={"/products"}>Products1</Link>
+      </Row>
+      <Row>{jokeMessage}</Row>
+      <Row>
+        <Link href={"/blog"}>Blog1</Link>
+      </Row>
+      <Row>
+        <Link href={"/products"}>Products1</Link>
+      </Row>
+      <Row>{jokeMessage}</Row>
+      <Row>
+        <Link href={"/blog"}>Blog1</Link>
+      </Row>
+      <Row>
+        <Link href={"/products"}>Products1</Link>
+      </Row>
     </Container>
   );
 }
